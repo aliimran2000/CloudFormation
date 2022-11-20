210c64ad28b5286adefd06f30509379044ad29fe
aliirns <ali.imran@rnssol.com>
2022-11-20 09:53:32 +0500

    script uncommented

--- script.sh
+++ script.sh
@@ -1,28 +1,29 @@
 #! /bin/bash
 
-# VAR1=$(aws cloudformation create-stack --stack-name S3STACK --template-body file://S3/template.yaml --parameters file://S3/parameters.json --output text )
-# VAR2=$(aws cloudformation create-stack --stack-name RDSSTACK --template-body file://RDS/template.yaml --parameters file://RDS/parameters.json --output text )
-# aws cloudformation wait stack-create-complete --stack-name $VAR1
-# echo "S3 Bucket Created"
-# #---------------------------------------------------
-# #fetching bucket name
+VAR1=$(aws cloudformation create-stack --stack-name S3STACK --template-body file://S3/template.yaml --parameters file://S3/parameters.json --output text )
+VAR2=$(aws cloudformation create-stack --stack-name RDSSTACK --template-body file://RDS/template.yaml --parameters file://RDS/parameters.json --output text )
+aws cloudformation wait stack-create-complete --stack-name $VAR1
+echo "S3 Bucket Created"
+#---------------------------------------------------
+#fetching bucket name
 BUCKET_NAME=$(aws cloudformation describe-stacks --stack-name S3STACK --query "Stacks[0].Outputs[0].OutputValue")
 BUCKET_NAME=${BUCKET_NAME%*\"}
 BUCKET_NAME=${BUCKET_NAME#*\"}
 BUCKET_NAME_UPLOAD="s3://${BUCKET_NAME}/"
-# echo "Uploading to S3 bucket"
-# aws s3 cp SampleDatabase.zip $BUCKET_NAME_UPLOAD
+
+
+echo "Uploading to S3 bucket"
+aws s3 cp SampleDatabase.zip $BUCKET_NAME_UPLOAD
 echo "-------------------------------->"
 echo "Uploaded database.zip to s3"
 DOWNLOAD_URL="https://${BUCKET_NAME}.s3.amazonaws.com/SampleDatabase.zip"
-# echo "Download zip from : ${DOWNLOAD_URL}"
+echo "Download zip from : ${DOWNLOAD_URL}"
 
 
-# echo "Waiting on AWS RDS"
-# ##------------------------------------------------->
-# aws cloudformation wait stack-create-complete --stack-name $VAR2
-# echo "MYSQL RDS Created"
-# echo "provsioning EC2 ..."
+echo "Waiting on AWS RDS"
+##------------------------------------------------->
+aws cloudformation wait stack-create-complete --stack-name $VAR2
+echo "MYSQL RDS Created"
 
 
 DB_HOST=$(aws cloudformation describe-stacks --stack-name RDSSTACK --query "Stacks[0].Outputs[0].OutputValue")
@@ -31,12 +32,16 @@ DB_HOST=${DB_HOST%*\"}
 echo $DB_HOST
 DB_HOST=${DB_HOST#*\"}
 
+
 ### PARAMETERS JSON
-
+echo "setting params for EC2 provisioning"
 node params.js $DOWNLOAD_URL $DB_HOST
-
+echo "params configured"
 ##------------------------------------------------->
+echo "provsioning EC2 ..."
 EC2=$(aws cloudformation create-stack --stack-name EC2Stack --template-body file://EC2/template.yaml --parameters file://EC2/parameters.json --output text)
+echo "EC2 Stack has been initiated : ${EC2}"
+echo "Waiting for Succesfuly Creation"
 aws cloudformation wait stack-create-complete --stack-name $EC2
-echo "Script complete"
+echo "All resources were created succesfully"
 
